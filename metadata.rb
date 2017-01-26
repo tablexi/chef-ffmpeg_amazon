abdc966abd4b4f1b1d0bd3f960d04fde6e151b5e
Lloyd Philbrook <lloyd@tablexi.com>
2017-01-25 18:28:08 -0600

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sysadmins@tablexi.com'
 license 'MIT'
 description 'Installs/Configures FFMPEG'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '0.0.2'
 
 supports 'amazon'
 
